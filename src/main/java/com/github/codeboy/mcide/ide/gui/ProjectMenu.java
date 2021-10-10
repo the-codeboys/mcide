58bb475892cf579b2c40aef621f2fff4733e58b6
ThreshMain <martinkos007@gmail.com>
2021-10-10 20:19:40 +0200

    ProjectMenu using MultiPage Gui for "unlimited" file support

--- src/main/java/com/github/codeboy/mcide/ide/gui/ProjectMenu.java
+++ src/main/java/com/github/codeboy/mcide/ide/gui/ProjectMenu.java
@@ -5,30 +5,41 @@ import com.github.codeboy.mcide.config.Message;
 import com.github.codeboy.mcide.ide.CodeProject;
 import com.github.codeboy.mcide.ide.MCCodeFile;
 import ml.codeboy.bukkitbootstrap.gui.Gui;
+import ml.codeboy.bukkitbootstrap.gui.MultiPageGui;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 
-public class ProjectMenu extends Gui {
+public class ProjectMenu extends MultiPageGui {
     private final CodeProject project;
 
     public ProjectMenu(CodeProject project) {
-        super(Mcide.getPlugin(Mcide.class), 54, project.getTitle());
+        super(Mcide.getPlugin(Mcide.class), 54, project.getTitle(), (Gui page) -> {
+            page.addItem(createItem(Material.ARROW, Message.BACK), 45, p -> new ProjectSelector(p).open(p));
+            page.addItem(createItem(Material.BANNER, Message.RUN), 46, project::run);
+            page.addItem(createItem(Material.WORKBENCH, Message.CREATE_FILE), 47, project::addFile);
+            page.addItem(createItem(Material.BARRIER, Message.DELETE_FILE), 48, project::removeFile);
+
+            page.addItemLast(createItem(Material.BOOK_AND_QUILL, "STDIN"), p -> project.editFile(project.getInput(), p));
+            page.addItemLast(createItem(Material.BOOK_AND_QUILL, "ARGS"), p -> project.editFile(project.getArgs(), p));
+        });
         this.project = project;
         ArrayList<MCCodeFile> mcCodeFiles = project.getMCCodeFiles();
         for (int i = 0, mcCodeFilesLength = mcCodeFiles.size(); i < mcCodeFilesLength; i++) {
             MCCodeFile file = mcCodeFiles.get(i);
             addFile(file, i == 0);
         }
+    }
 
-        addItem(createItem(Material.ARROW, Message.BACK), 45, p -> new ProjectSelector(p).open(p));
-        addItem(createItem(Material.BANNER, Message.RUN), 46, project::run);
-        addItem(createItem(Material.WORKBENCH, Message.CREATE_FILE), 47, project::addFile);
-        addItem(createItem(Material.BARRIER, Message.DELETE_FILE), 48, project::removeFile);
+    @Override
+    protected int getNextButtonIndex() {
+        return 51;
+    }
 
-        addItemLast(createItem(Material.BOOK_AND_QUILL, "STDIN"), p -> project.editFile(project.getInput(), p));
-        addItemLast(createItem(Material.BOOK_AND_QUILL, "ARGS"), p -> project.editFile(project.getArgs(), p));
+    @Override
+    protected int getPrevButtonIndex() {
+        return 50;
     }
 
     private void addFile(MCCodeFile file, boolean mainFile) {
